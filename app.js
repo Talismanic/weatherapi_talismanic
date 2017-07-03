a4b22bdd572f2c3c75c024b4733c16689e2c0e65
Mohammed Mehedi Hasan <u.urs.888@gmail.com>
2017-07-03 14:33:49 +0600

    fourth commit
    fourth commit

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-
+var weather= require('./routes/weather');
 var app = express();
 
 // view engine setup
@@ -17,13 +17,14 @@ app.set('view engine', 'pug');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
+app.use(bodyParser.json({type: 'application/json'}));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/weather', weather);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
